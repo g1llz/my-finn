d7512c1f307d36c9d580689027b0cda665e5c417
Gilles Lima <gillestoim@gmail.com>
2019-06-29 18:48:05 -0300

    create user test file

---
+++ test/user.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const server = require('../src/server');
+
+test('Should be list all users', () => request(server).get('/users')
+  .then((res) => {
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveLength(1);
+    expect(res.body[0]).toHaveProperty('name', 'Bruce Wayne');
+  }));
+
+test('Should be insert an user', () => request(server).post('/users')
+  .send({ name: 'Walter Mitty', email: 'walter@mitty.com' })
+  .then((res) => {
+    expect(res.status).toBe(201);
+    expect(res.body.name).toBe('Walter Mitty');
+  }));

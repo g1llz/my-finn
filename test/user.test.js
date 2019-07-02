3d1bbae04beac95031ad23b0e72dd765dbef479c
Gilles Lima <gillestoim@gmail.com>
2019-07-01 23:46:31 -0300

    update user test

--- test/user.test.js
+++ test/user.test.js
@@ -1,16 +1,23 @@
 const request = require('supertest');
 const server = require('../src/server');
 
-test('Should list all users', () => request(server).get('/users')
+const email = `${Date.now()}@mail.com`;
+
+test('Should return status code 200 and []', () => request(server).get('/users')
   .then((res) => {
     expect(res.status).toBe(200);
-    expect(res.body).toHaveLength(1);
-    expect(res.body[0]).toHaveProperty('name', 'Bruce Wayne');
+    expect(res.body).toHaveLength(0);
   }));
 
 test('Should insert a user', () => request(server).post('/users')
-  .send({ name: 'Walter Mitty', email: 'walter@mitty.com' })
+  .send({ name: 'Walter Mitty', email, password: '123456' })
   .then((res) => {
     expect(res.status).toBe(201);
     expect(res.body.name).toBe('Walter Mitty');
+  }));
+
+test('Should list all users', () => request(server).get('/users')
+  .then((res) => {
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
   }));

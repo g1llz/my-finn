c036fcb72fb54f716c554a0b944fca8bf38429ba
Gilles Lima <gillestoim@gmail.com>
2019-07-01 23:46:21 -0300

    inject knexfile in controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,7 @@
-const user = require('../controllers/user');
+const knex = require('knex');
+const db = knex(require('../../knexfile').test);
+const user = require('../controllers/user')(db);
+
 
 const routes = (server) => {
   server.get('/users', user.list);

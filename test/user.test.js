7966178257465bfeaed76e552338d8085134b83a
Gilles Lima <gillestoim@gmail.com>
2019-07-04 00:03:41 -0300

    update users test

--- test/user.test.js
+++ test/user.test.js
@@ -1,7 +1,17 @@
 const request = require('supertest');
+const knex = require('knex');
 const server = require('../src/server');
 
-const email = `${Date.now()}@mail.com`;
+const db = knex(require('../knexfile').test);
+
+const truncate = table => db.raw(`TRUNCATE TABLE ${table}`);
+
+beforeAll(() => {
+  console.log('=> TRUNCATE');
+  return truncate('users');
+});
+
+const email = () => `${Date.now()}@mail.com`;
 
 test('Should return status code 200 and []', () => request(server).get('/users')
   .then((res) => {
@@ -10,7 +20,7 @@ test('Should return status code 200 and []', () => request(server).get('/users')
   }));
 
 test('Should insert a user', () => request(server).post('/users')
-  .send({ name: 'Walter Mitty', email, password: '123456' })
+  .send({ name: 'Walter Mitty', email: email(), password: '123456' })
   .then((res) => {
     expect(res.status).toBe(201);
     expect(res.body.name).toBe('Walter Mitty');
@@ -20,4 +30,20 @@ test('Should list all users', () => request(server).get('/users')
   .then((res) => {
     expect(res.status).toBe(200);
     expect(res.body.length).toBeGreaterThan(0);
+  }));
+
+test('Should not save if name is null', () => request(server).post('/users')
+  .send({ email: email(), password: '123456' })
+  .then((res) => {
+    expect(res.status).toBe(400);
+    expect(res.body.error.code).toBe('23502');
+    expect(res.body.error.message).toBe('name not-null');
+  }));
+
+test('Should not save if name is empty', () => request(server).post('/users')
+  .send({ name: '', email: email(), password: '123456' })
+  .then((res) => {
+    expect(res.status).toBe(400);
+    expect(res.body.error.code).toBe('NA');
+    expect(res.body.error.message).toBe('all attributes are required');
   }));

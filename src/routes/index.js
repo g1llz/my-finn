bc7afbcc948a95ff4d6f3235b8d8c0aba581a2ca
Gilles Lima <gillestoim@gmail.com>
2019-09-13 00:50:25 -0300

    update routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,11 @@
 const knex = require('knex');
 const db = knex(require('../../knexfile').test);
 const user = require('../controllers/user')(db);
-const account = require('../controllers/account')(db);
-
 
 const routes = (server) => {
   server.get('/users', user.list);
   server.post('/users', user.create);
-
-  server.post('/accounts', account.create);
+  server.delete('/users/:id', user.remove);
 };
 
 module.exports = routes;

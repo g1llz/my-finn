8bb62110aa491c9d659d8038bca5052f575bdfc1
Gilles Lima <gillestoim@gmail.com>
2019-09-14 22:31:55 -0300

    create dev environment

--- knexfile.js
+++ knexfile.js
@@ -13,4 +13,17 @@ module.exports = {
       directory: 'src/migrations',
     },
   },
+  dev: {
+    client: 'pg',
+    version: '9.6',
+    connection: {
+      host: 'localhost',
+      user: 'postgres',
+      password: '',
+      database: 'my_finn_dev',
+    },
+    migrations: {
+      directory: 'src/migrations',
+    },
+  },
 };

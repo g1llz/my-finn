318ac19165e74253c8cb170f97956cbdadb0b713
Gilles Lima <gillestoim@gmail.com>
2019-09-16 22:45:36 -0300

    enable cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,9 @@
 const server = require('express')();
+const cors = require('cors');
+
 const routes = require('../routes');
 
+server.use(cors());
 server.use(require('body-parser').json());
 
 server.get('/', (req, res) => {
